d1e964c08900242cb45b5961e6d24a4bb77a54b9
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-20 13:56:45 +0300

    created <Tooltip /> for each option in Lights options

--- src/ctrls/CtrlLights/const/options.tsx
+++ src/ctrls/CtrlLights/const/options.tsx
@@ -1,7 +1,35 @@
-import type { ListBoxItem } from '@project-1114/ui-kit';
+import {
+    Card,
+    HStack,
+    Icon,
+    type ListBoxItem,
+    Tooltip,
+} from '@project-1114/ui-kit';
 import { Text } from '@project-1114/ui-kit';
 import { lightMods } from './lightMods.ts';
+import InfoIcon from '../assets/info.svg?react';
 
 export const options: ListBoxItem<string>[] = Object.entries(lightMods).map(
-    ([value, mode]) => ({ value, content: <Text>{mode.label}</Text> }),
+    ([value, mode]) => ({
+        value,
+        content: (
+            <HStack align={'center'} justify={'between'} gap={'l'}>
+                <Text>{mode.label}</Text>
+                <Tooltip
+                    content={
+                        <Card variant={'transparent'} style={{ width: 300 }}>
+                            <Text>{mode.description}</Text>
+                        </Card>
+                    }
+                >
+                    <Icon
+                        Svg={InfoIcon}
+                        fill={'var(--color-secondary)'}
+                        width={16}
+                        height={16}
+                    />
+                </Tooltip>
+            </HStack>
+        ),
+    }),
 );
