460c43304bc1aef315430dd7049aedc08c0362a2
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-29 00:47:21 +0300

    decomposed api logic for <CtrlLights />

--- src/ctrls/CtrlLights/ui/CtrlLights.tsx
+++ src/ctrls/CtrlLights/ui/CtrlLights.tsx
@@ -1,9 +1,22 @@
-import { HStack, Text } from '@project-1114/ui-kit';
+import { HStack, Skeleton, Text } from '@project-1114/ui-kit';
 import { ListLightMods } from '@/ctrls/CtrlLights/ui/ListLightMods.tsx';
+import { useTriggerClientEventQuery } from '@project-1114/platform-frontend';
+import { ELightMods } from '@/ctrls/CtrlLights/const/ELightMods.ts';
 
-export const CtrlLights = () => (
-    <HStack align={'center'} gap={'m'}>
-        <Text size={'l'}>Фары:</Text>
-        <ListLightMods />
-    </HStack>
-);
+export const CtrlLights = () => {
+    const { data, isLoading, error } = useTriggerClientEventQuery<ELightMods>(
+        'f:c:getLightsState',
+        'c:f:getLightsState',
+    );
+
+    if (isLoading)
+        return <Skeleton width={'100%'} height={40} border={'8px'} />;
+    else if (error) return <Text>{error}</Text>;
+    else if (data === null) return <Text>no data</Text>;
+    return (
+        <HStack align={'center'} gap={'m'}>
+            <Text size={'l'}>Фары:</Text>
+            <ListLightMods initialValue={data} />
+        </HStack>
+    );
+};
--- src/ctrls/CtrlLights/ui/ListLightMods.tsx
+++ src/ctrls/CtrlLights/ui/ListLightMods.tsx
@@ -1,40 +1,26 @@
 import { ListBox, Text } from '@project-1114/ui-kit';
 
-import {
-    useTriggerClientEventMutation,
-    useTriggerClientEventQuery,
-} from '@project-1114/platform-frontend';
+import { useTriggerClientEventMutation } from '@project-1114/platform-frontend';
 import { options } from '@/ctrls/CtrlLights/const/options.tsx';
 import { ELightMods } from '@/ctrls/CtrlLights/const/ELightMods.ts';
+import type { FC } from 'react';
 
-export const ListLightMods = () => {
-    const {
-        data: lightsState,
-        isLoading: lightsStateIsLoading,
-        error: lightsStateError,
-    } = useTriggerClientEventQuery<ELightMods>(
-        'f:c:getLightsState',
-        'c:f:getLightsState',
-    );
-
+type ListLightModsProps = {
+    initialValue: ELightMods;
+};
+export const ListLightMods: FC<ListLightModsProps> = ({ initialValue }) => {
     const [handle, { data, isLoading, error }] = useTriggerClientEventMutation<
         ELightMods,
         ELightMods
     >('f:c:setLightsState', 'c:f:setLightsState');
 
-    if (lightsStateError) return <Text>{lightsStateError}</Text>;
-    else if (error) return <Text>{error}</Text>;
-
-    const handleSwitchLightsMode = (value: ELightMods) => {
-        handle(value);
-    };
-
+    if (error) return <Text>{error}</Text>;
     return (
         <ListBox
-            value={data || lightsState}
-            onChange={handleSwitchLightsMode}
+            value={data || initialValue}
+            onChange={(value) => handle(value)}
             items={options}
-            readonly={lightsStateIsLoading || isLoading}
+            readonly={isLoading}
         />
     );
 };
