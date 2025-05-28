18215ff87ae21103f48119ffc0b814f6d67c3063
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-29 00:41:20 +0300

    decomposed api logic for <CtrlEngine />

--- src/ctrls/CtrlEngine/ui/CtrlEngine.tsx
+++ src/ctrls/CtrlEngine/ui/CtrlEngine.tsx
@@ -1,11 +1,20 @@
-import { HStack, Text } from '@project-1114/ui-kit';
+import { HStack, Skeleton, Text } from '@project-1114/ui-kit';
 import { EngineSwitcher } from '@/ctrls/CtrlEngine/ui/EngineSwitcher.tsx';
+import { useTriggerClientEventQuery } from '@project-1114/platform-frontend';
 
 export const CtrlEngine = () => {
+    const { data, isLoading, error } = useTriggerClientEventQuery<boolean>(
+        'f:c:getEngineState',
+        'c:f:getEngineState',
+    );
+
+    if (isLoading) return <Skeleton width={32} height={64} border={'50%'} />;
+    else if (error) return <Text>{error}</Text>;
+    else if (data === null) return <Text>no data</Text>;
     return (
         <HStack align={'center'} gap={'m'}>
             <Text size={'l'}>Двигатель:</Text>
-            <EngineSwitcher />
+            <EngineSwitcher initialValue={data} />
         </HStack>
     );
 };
--- src/ctrls/CtrlEngine/ui/EngineSwitcher.tsx
+++ src/ctrls/CtrlEngine/ui/EngineSwitcher.tsx
@@ -1,48 +1,24 @@
 import type { FC } from 'react';
 import { Switcher, Text } from '@project-1114/ui-kit';
-import {
-    useTriggerClientEventQuery,
-    useTriggerClientEventMutation,
-} from '@project-1114/platform-frontend';
+import { useTriggerClientEventMutation } from '@project-1114/platform-frontend';
 
-export const EngineSwitcher: FC = () => {
-    const {
-        data: engineState,
-        isLoading: engineStateIsLoading,
-        error: engineStateError,
-    } = useTriggerClientEventQuery<boolean>(
-        'f:c:getEngineState',
-        'c:f:getEngineState',
-    );
+type EngineSwitcherProps = {
+    initialValue: boolean;
+};
+export const EngineSwitcher: FC<EngineSwitcherProps> = ({ initialValue }) => {
+    const [handle, { data, isLoading, error }] =
+        useTriggerClientEventMutation<boolean>(
+            'f:c:toggleEngineState',
+            'c:f:toggleEngineState',
+        );
 
-    const [
-        handle,
-        {
-            data: toggledEngineState,
-            isLoading: toggledEngineIsLoading,
-            error: toggledEngineStateError,
-        },
-    ] = useTriggerClientEventMutation<boolean>(
-        'f:c:toggleEngineState',
-        'c:f:toggleEngineState',
-    );
-
-    const state = toggledEngineState || engineState;
-
-    if (engineStateError) return <Text>{engineStateError}</Text>;
-    else if (toggledEngineStateError)
-        return <Text>{toggledEngineStateError}</Text>;
-
-    const handleSwitch = () => {
-        handle(null);
-    };
-
+    if (error) return <Text>{error}</Text>;
     return (
         <Switcher
             id={'engine'}
-            value={state!}
-            onChange={handleSwitch}
-            disabled={engineStateIsLoading || toggledEngineIsLoading}
+            value={data || initialValue}
+            onChange={() => handle(null)}
+            disabled={isLoading}
         />
     );
 };
